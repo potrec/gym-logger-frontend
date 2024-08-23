df421900fcc815b92c3723c9bb0b9f61c16216e0
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-08-23 19:41:39 +0200

    :feat add icons to the input component

--- gym-logger-frontend/src/views/ComponentTestView.vue
+++ gym-logger-frontend/src/views/ComponentTestView.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="flex w-screen h-screen items-center justify-center">
     <div class="flex flex-col w-1/2 h-1/2 justify-evenly bg-page-accent p-6">
-      <Input label="Username" type="text" id="username" />
-      <Input label="Email" type="email" id="email" />
-      <Input label="Password" type="password" id="password" />
-      <Input label="Confirm Password" type="password" id="confirmPassword" />
+      <Input label="Username" type="text" id="username" :icon="['fas', 'user']" />
+      <Input label="Email" type="email" id="email" :icon="['fas', 'at']" />
+      <Input label="Password" type="password" id="password" :icon="['fas', 'lock']" />
+      <Input
+        label="Confirm Password"
+        type="password"
+        id="confirmPassword"
+        :icon="['fas', 'user']"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Input from '@/components/common/Input.vue'
+import Input from '@/components/common/InputComponent.vue'
 </script>
