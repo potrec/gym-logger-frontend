338c4a89e3103da9ebbffb9e69724cb9d5cd59f8
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-08-23 03:24:51 +0200

    feat: add register view

--- gym-logger-frontend/src/views/RegisterView.vue
+++ gym-logger-frontend/src/views/RegisterView.vue
@@ -1,5 +1,47 @@
 <template>
-  <div>Register</div>
+  <div class="flex justify-center w-screen h-screen items-center text-black">
+    <div class="flex flex-col bg-panel items-center m-16 rounded-md w-3/4 lg:w-3/8 xl:w-1/4">
+      <h1 class="text-4xl mt-4 font-bold">Register</h1>
+      <div class="flex flex-col w-1/2 rounded-lg mt-6">
+        <label for="username">Username</label>
+        <input
+          class="mt-1 border-2 border-solid border-slate-600 rounded-lg pl-2"
+          type="text"
+          id="username"
+        />
+      </div>
+      <div class="flex flex-col w-1/2 mt-6">
+        <label for="email">Email</label>
+        <input
+          class="mt-1 border-2 border-solid border-slate-600 rounded-lg pl-2"
+          type="email"
+          id="email"
+        />
+      </div>
+      <div class="flex flex-col w-1/2 mt-6">
+        <label for="password">Password</label>
+        <input
+          class="mt-1 border-2 border-solid border-slate-600 rounded-lg pl-2"
+          type="password"
+          id="password"
+        />
+      </div>
+      <div class="flex flex-col w-1/2 mt-6">
+        <label for="confirmPassword">Confirm Password</label>
+        <input
+          class="mt-1 border-2 border-solid border-slate-600 rounded-lg pl-2"
+          type="password"
+          id="confirmPassword"
+        />
+      </div>
+      <button
+        class="bg-primary text-white pt-2 px-6 rounded-md border-2 border-solid border-slate-600 mt-6"
+      >
+        Register
+      </button>
+      Already have an account? <router-link to="/login">Login</router-link>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
