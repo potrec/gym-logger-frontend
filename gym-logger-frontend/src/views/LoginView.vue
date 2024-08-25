95d5345e515ac9607a4e9852f77fef7bba738bf1
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-08-26 01:54:10 +0200

    ref: change inputs in login and register view

--- gym-logger-frontend/src/views/LoginView.vue
+++ gym-logger-frontend/src/views/LoginView.vue
@@ -1,37 +1,21 @@
 <template>
-  <div class="flex justify-center w-screen h-screen items-center text-black">
-    <div class="flex flex-col bg-panel items-center m-16 rounded-md w-3/4 lg:w-3/8 xl:w-1/4">
-      <h1 class="text-4xl mt-4 font-bold">Login</h1>
-      <div class="flex flex-col w-1/2 rounded-lg mt-6">
-        <label for="email">Email</label>
-        <input
-          class="mt-1 border-2 border-solid border-slate-600 rounded-lg pl-2"
-          type="email"
-          id="email"
-        />
+  <div class="flex shadow-2xl flex-col">
+    <div class="flex">
+      <div class="flex flex-col w-1/2 h-1/2 justify-evenly bg-page-accent p-6 w-3/5">
+        <Input label="Email" type="email" id="email" :icon="['fas', 'at']" />
+        <Input label="Password" type="password" id="password" :icon="['fas', 'lock']" />
+        <button class="bg-primary text-white py-2 px-4 rounded">Login</button>
       </div>
-      <div class="flex flex-col w-1/2 mt-6 mb-6">
-        <label for="password">Password</label>
-        <input
-          class="mt-1 border-2 border-solid border-slate-600 rounded-lg pl-2"
-          type="password"
-          id="password"
-        />
+      <div class="bg-primary-accent w-2/5">
+        <h1 class="text-4xl text-white mx-2 my-2">Welcome</h1>
       </div>
-      <button
-        class="bg-primary text-white pt-2 px-6 rounded-md border-2 border-solid border-slate-600"
-      >
-        Login
-      </button>
-      Don't have an account? <router-link to="/register">Register</router-link>
+    </div>
+    <div class="bg-primary-accent text-sm text-white p-2">
+      Don't have an account? <router-link to="/register">Click here to register</router-link>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'LoginView'
-}
+<script setup lang="ts">
+import Input from '@/components/common/InputComponent.vue'
 </script>
-
-<style scoped></style>
--- gym-logger-frontend/src/views/RegisterView.vue
+++ gym-logger-frontend/src/views/RegisterView.vue
@@ -1,53 +1,27 @@
 <template>
-  <div class="flex justify-center w-screen h-screen items-center text-black">
-    <div class="flex flex-col bg-panel items-center m-16 rounded-md w-3/4 lg:w-3/8 xl:w-1/4">
-      <h1 class="text-4xl mt-4 font-bold">Register</h1>
-      <div class="flex flex-col w-1/2 rounded-lg mt-6">
-        <label for="username">Username</label>
-        <input
-          class="mt-1 border-2 border-solid border-slate-600 rounded-lg pl-2"
-          type="text"
-          id="username"
-        />
-      </div>
-      <div class="flex flex-col w-1/2 mt-6">
-        <label for="email">Email</label>
-        <input
-          class="mt-1 border-2 border-solid border-slate-600 rounded-lg pl-2"
-          type="email"
-          id="email"
-        />
-      </div>
-      <div class="flex flex-col w-1/2 mt-6">
-        <label for="password">Password</label>
-        <input
-          class="mt-1 border-2 border-solid border-slate-600 rounded-lg pl-2"
-          type="password"
-          id="password"
-        />
-      </div>
-      <div class="flex flex-col w-1/2 mt-6">
-        <label for="confirmPassword">Confirm Password</label>
-        <input
-          class="mt-1 border-2 border-solid border-slate-600 rounded-lg pl-2"
+  <div class="flex shadow-2xl flex-col">
+    <div class="flex">
+      <div class="flex flex-col w-1/2 h-1/2 justify-evenly bg-page-accent p-6 w-3/5">
+        <Input label="Username" type="text" id="username" :icon="['fas', 'user']" />
+        <Input label="Email" type="email" id="email" :icon="['fas', 'at']" />
+        <Input label="Password" type="password" id="password" :icon="['fas', 'lock']" />
+        <Input
+          label="Confirm Password"
           type="password"
           id="confirmPassword"
+          :icon="['fas', 'user']"
         />
       </div>
-      <button
-        class="bg-primary text-white pt-2 px-6 rounded-md border-2 border-solid border-slate-600 mt-6"
-      >
-        Register
-      </button>
-      Already have an account? <router-link to="/login">Login</router-link>
+      <div class="bg-primary-accent w-2/5">
+        <h1 class="text-4xl text-white mx-2 my-2">Welcome</h1>
+      </div>
+    </div>
+    <div class="bg-primary-accent text-sm text-white p-2">
+      You have an account? <router-link to="/register">Click here to login</router-link>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'RegisterView'
-}
+<script setup lang="ts">
+import Input from '@/components/common/InputComponent.vue'
 </script>
-
-<style scoped></style>
