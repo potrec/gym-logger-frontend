bac1033b61ffe48ae2ac93d28c28bcfaeec43579
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-09-02 02:18:54 +0200

    feat: add validator for user login

---
+++ gym-logger-backend/Validators/User/UserLoginValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using gym_logger_backend.Dto.User;
+
+namespace gym_logger_backend.Validators.User
+{
+    public class UserLoginValidator : AbstractValidator<UserLoginDto>
+    {
+        public UserLoginValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not valid");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
+            RuleFor(x => x.Password).MinimumLength(6).WithMessage("Password must be at least 8 characters");
+
+        }
+    }
+}
